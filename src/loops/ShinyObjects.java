4cdcfb3dcfc4a8f443b1f82d2d01f478b71598d0
victoriagonzalez58 <Victoria.Gonzalez-Calderon@gompersstudent.org>
2017-10-16 14:24:44 -0700

    Add files via upload

--- src/loops/ShinyObjects.java
+++ src/loops/ShinyObjects.java
@@ -5,28 +5,37 @@ package loops;
 
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.swing.JOptionPane;
 
 
 public class ShinyObjects {
 	public static void main(String[] args) {
 
 		// 2. Ask the user how many shiny objects they want
+		String som = JOptionPane.showInputDialog(null, "How many shiny objects do you want?");
 
-		// 3. Play the sound that many times
+		int some = Integer.parseInt(som);
+		for(int i =1;i<=some;i++){
+		
+		}
+		}
+				// 3. Play the sound that many times
+
 
 		// 1. Call the method below
 
-	}
+	
 
 	public static void playMisterZee() {
+		
     	try {
  
    		 Clip clip = AudioSystem.getClip();
    		 clip.open(AudioSystem.getAudioInputStream(ShinyObjects.class.getResource("shiny-objects.wav")));
    		 clip.start();
    		 Thread.sleep(3600);
-    	} catch (Exception ex) {
-        	ex.printStackTrace();
+    	} catch (Exception e) {
+        	e.printStackTrace();
     	}
 	}
 
--- src/loops/WalkOfFame.java
+++ src/loops/WalkOfFame.java
@@ -8,7 +8,43 @@ import org.jointheleague.graphical.robot.Robot;
 
 public class WalkOfFame {
 	public static void main(String[] args) {
+		
 		Robot rob = new Robot();
+		rob.moveTo(10, 300);
+		rob.penDown();
+		rob.setSpeed(100);
+		
+		rob.setRandomPenColor();
+		rob.setPenWidth(30);
+		rob.turn(90);
+		
+				
+		for (int i2 = 0; i2 < 5; i2++) {
+			rob.penDown();
+			rob.setSpeed(100);
+			rob.setRandomPenColor();
+			rob.setPenWidth(30);
+			
+			rob.move(100);
+			rob.turn(144);
+		}
+rob.penUp();	
+rob.move(100);
+rob.turn(270);
+rob.move(50);
+for (int i2 = 0; i2 < 5; i2++) {
+	rob.penDown();
+	rob.setSpeed(100);
+	rob.setRandomPenColor();
+	
+	rob.setPenWidth(30);
+	rob.move(100);
+	rob.turn(144);
+}
+
+}
+	}
+		
 		// 1. Set the X position of the robot so that it starts on the left. 
 		// You also need to show the robot to see the result of this line.
 
@@ -19,11 +55,12 @@ public class WalkOfFame {
 		/** THE CHALLENGE: **/
 		/*
 		 * Make the robot draw a line of stars like this:
-		 * http://bit.ly/walk-of-fame
+		 * * http://bit.ly/walk-of-fame
+e robot draw a line of stars like this:
 		 * 
 		 * Hint: The distance between stars is 50.
 		 */
 
-	}
+	
 
-}
+
