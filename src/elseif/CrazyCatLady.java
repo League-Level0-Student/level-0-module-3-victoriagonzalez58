85b4263550cf43f00c0d2b556bd6f8ba9687fc78
victoriagonzalez58 <Victoria.Gonzalez-Calderon@gompersstudent.org>
2017-11-06 14:23:58 -0800

    Add files via upload

--- src/elseif/CrazyCatLady.java
+++ src/elseif/CrazyCatLady.java
@@ -5,20 +5,9 @@ package elseif;
 
 import java.net.URI;
 
+import javax.swing.JOptionPane;
+
 public class CrazyCatLady {
-	public static void main(String[] args) {
-		// 1. Ask the user how many cats they have
-
-		// 2. Convert their answer into an int
-
-		// 3. If they have more than 3 cats, tell them they're a crazy cat lady
-
-		// 4. If they have 3 or less, call the method below to show them a cat video
-
-		// 5. If they say 0, show them a video of A Frog Sitting on a Bench Like a Human
-		
-	}
-
 	static void playVideo(String videoURL) {
 		try {
 			URI uri = new URI(videoURL);
@@ -27,6 +16,36 @@ public class CrazyCatLady {
 			e.printStackTrace();
 		}
 	}
+	public static void main(String[] args) {
+	
+		// 1. Ask the user how many cats they have
+String ans = JOptionPane.showInputDialog(null, "How many cats do you have?");
+		// 2. Convert their answer into an int
+int anss =Integer.parseInt(ans);
+		// 3. If they have more than 3 cats, tell them they're a crazy cat lady
+
+if(anss >=3){
+	JOptionPane.showMessageDialog(null, "You're crazy");
+}if(anss ==0){
+	JOptionPane.showMessageDialog(null, "jds");
+	
+}if(anss <=3){
+	JOptionPane.showMessageDialog(null, "You're crazy");
+	playVideo();
+}
+
 
 }
+	}
+
+
+		// 4. If they have 3 or less, call the method below to show them a cat video
+
+		// 5. If they say 0, show them a video of A Frog Sitting on a Bench Like a Human
+		
+	
+
+
+
+
 
